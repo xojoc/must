5219ef2342470611b74613bf1cd117b3e4aaf14a
xojoc <xojoc@gmx.com>
2017-04-29 13:54:25 +0200

    s

---
+++ README.md
@@ -0,0 +1,27 @@
+# MUST
+*MUST* is a library written in [golang](https://golang.org) implementing a **must** version of common functions.
+
+# Usage
+Install the library with
+```
+go get xojoc.pw/must
+```
+
+...then run...
+
+````
+go test
+``````
+
+if something fails open an issue.
+
+See [godoc](https://godoc.org/xojoc.pw/must) for the complete documentation.
+
+# Who?
+*MUST* was written by Alexandru Cojocaru (https://xojoc.pw).
+
+# [Donate!](https://liberapay.com/xojoc)
+
+# License
+*bitset* is [Free Software](https://www.gnu.org/philosophy/free-sw.html) and is released under the Apache 2.0 license. No warranty.
+
---
+++ must.go
@@ -0,0 +1,83 @@
+// Written by https://xojoc.pw. Apache license 2.0
+
+// Package must provides "must" variants of common functions.
+//
+// These functions are handy in main programs and tests.
+package must // import "xojoc.pw/must"
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"path"
+	"runtime"
+)
+
+// OK prints a stack trace and calls log.Fatal if err != nil.
+// Otherwise does nothing.
+func OK(err error) {
+	if err == nil {
+		return
+	}
+	buf := make([]byte, 8000)
+	runtime.Stack(buf, false)
+	fmt.Println(string(buf))
+	log.Fatal(err)
+}
+
+func Open(path string) *os.File {
+	file, err := os.Open(path)
+	OK(err)
+	return file
+}
+
+func Close(c io.Closer) {
+	OK(c.Close())
+}
+
+func Create(path string) *os.File {
+	file, err := os.Create(path)
+	OK(err)
+	return file
+}
+
+func ReadImage(path string) image.Image {
+	img, _, err := image.Decode(bytes.NewBuffer(ReadFile(path)))
+	OK(err)
+	return img
+}
+
+func WriteImage(file string, img image.Image) {
+	ext := path.Ext(file)
+	switch ext {
+	case ".png":
+		f := Create(file)
+		OK(png.Encode(f, img))
+	default:
+		panic("unknown file extension：" + ext)
+	}
+}
+
+func ReadFile(path string) []byte {
+	d, err := ioutil.ReadFile(path)
+	OK(err)
+	return d
+}
+
+func ReadAll(r io.Reader) []byte {
+	b, err := ioutil.ReadAll(r)
+	OK(err)
+	return b
+}
+
+func URL(s string) *url.URL {
+	u, err := url.Parse(s)
+	OK(err)
+	return u
+}
